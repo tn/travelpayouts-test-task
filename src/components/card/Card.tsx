595578d3b1dbc5126bb4cdc99f04cfe7648fd1a4
tn <t.nurislamov@gmail.com>
2019-04-25 00:15:35 +0300

    Copy to clipboard

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -10,20 +10,40 @@ interface ICardProps {
   promo: string
 }
 
-const Card: React.SFC<ICardProps> = (props) => (
-  <div className='card'>
-    <div className='card__info'>
-      <h3 className='card__title'>
-        <a className='card__link' href={props.link} rel='noopener nofollow' target='_blank'>{props.title}</a>
-      </h3>
-      <div className='card__desc'>{props.desc}</div>
+const Card: React.SFC<ICardProps> = (props) => {
+  const inputRef = React.useRef() as any
+  const [isCopied, setCopied] = React.useState(false)
+
+  const onCopy = (event: React.MouseEvent<HTMLButtonElement>) => {
+    inputRef.current.focus()
+    inputRef.current.select()
+
+    try {
+      const copied = document.execCommand('copy')
+      setCopied(!!copied)
+      setTimeout(() => {
+        setCopied(false)
+      }, 3000)
+    } catch (err) {
+      console.error(err)
+    }
+  }
+
+  return (
+    <div className='card'>
+      <div className='card__info'>
+        <h3 className='card__title'>
+          <a className='card__link' href={props.link} rel='noopener nofollow' target='_blank'>{props.title}</a>
+        </h3>
+        <div className='card__desc'>{props.desc}</div>
+      </div>
+      <div className='card__action'>
+        <Input ref={inputRef} labelId='promocode' value={props.promo} readOnly={true}>Промокод {isCopied ? '(Скопировано в буфер обмена)' : ''}</Input>
+        <button className='card__copy' onClick={onCopy}>Copy</button>
+      </div>
+      <Button primary={true}>Получить бонус</Button>
     </div>
-    <div className='card__action'>
-      <Input labelId='promocode' value={props.promo} readOnly={true}>Промокод</Input>
-      <button className='card__copy'>Copy</button>
-    </div>
-    <Button primary={true}>Получить бонус</Button>
-  </div>
-)
+  )
+}
 
 export default Card
--- src/components/input/Input.tsx
+++ src/components/input/Input.tsx
@@ -7,13 +7,29 @@ interface IInputProps {
   readOnly?: boolean
   onChange?: React.ChangeEventHandler
   autoComplete?: string
+  ref?: any
 }
 
-const Input: React.SFC<IInputProps> = ({ children, labelId, ...rest }) => (
-  <React.Fragment>
-    <label className='field__label' htmlFor={labelId}>{children}</label>
-    <input className='field' id={labelId} {...rest} />
-  </React.Fragment>
-)
+let Input: React.SFC<IInputProps> = ({ children, labelId, ...rest }, ref) => {
+  const inputRef = React.useRef() as any
+
+  React.useImperativeHandle(ref, () => ({
+    focus: () => {
+      inputRef.current.focus()
+    },
+    select: () => {
+      inputRef.current.select()
+    }
+  }))
+
+  return (
+    <React.Fragment>
+      <label className='field__label' htmlFor={labelId}>{children}</label>
+      <input ref={inputRef} className='field' id={labelId} {...rest} />
+    </React.Fragment>
+  )
+}
+
+Input = React.forwardRef(Input)
 
 export default Input
